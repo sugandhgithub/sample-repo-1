a8aaa5852a4c420f04144aa08b38d66ecce201b3
sugandhgithub <sugandhgithub@gmail.com>
2019-06-25 01:06:24 -0700

    Added selenium package and browser file to launch

---
+++ launchbrowser.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome('/Users/neelamtawar/PycharmProjects/SampleGitProject/drivers/chromedriver')
+driver.get('https://www.google.com')
+driver.maximize_window()
+time.sleep(5)
+driver.quit()
+
+
